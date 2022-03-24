ef21cc086f4329df0532cbd49736fb5175bc31e5
Vasily Tchernousov <vtchernousov@gmail.com>
2022-03-24 11:11:33 +0200

    commit files

---
+++ pro-testing-master/lesson_01/home_assignments/Wordle_game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wordle_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
